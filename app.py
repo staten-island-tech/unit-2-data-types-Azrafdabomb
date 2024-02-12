47397e4c7aacdcf70092dcdcd2a160b040f422fb
Azrafdabomb <azrafh@nycstudents.net>
2024-02-12 13:59:39 -0500

    47

---
+++ app.py
@@ -0,0 +1,11 @@
+
+x = 4
+y = 4
+
+def check_if_4(x,y):
+    if ( x == 4, y == 4):
+        print ("yeah")
+    else:
+        print ("I seriously dont feel loved by anyone. I broke down last night and no one cared to listen to me")
+
+check_if_4()
