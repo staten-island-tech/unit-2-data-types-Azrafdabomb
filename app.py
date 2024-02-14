c5b8653546763471bde2d0591ad783956b1d96bc
Azrafdabomb <azrafh@nycstudents.net>
2024-02-14 13:58:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+y = int(input("Number 1: "))
+x = int(input("Number 2: "))
 
-x = 4
-y = 4
 
-def check_if_4(x,y):
-    if ( x == 4, y == 4):
-        print ("yeah")
-    else:
-        print ("I seriously dont feel loved by anyone. I broke down last night and no one cared to listen to me")
-
-check_if_4()
+def gcf:
+    y = int(input("Number 1: "))
+    x = int(input("Number 2: "))
+    if x/y == 1:
+        print("Type different numbers please")
