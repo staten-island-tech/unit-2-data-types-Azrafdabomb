244b43a957558855f1423950942f421a5b8462ed
Azrafdabomb <azrafh@nycstudents.net>
2024-02-27 13:48:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,23 @@ def gcf():
     y = int(input("Number 1: "))
     x = int(input("Number 2: "))
 
-commonfactors = []
+commonxfactors = []
+
+gcfah = []
 def xfactors():
     for i in range(1, x + 1):
-        if(x % i == 0):
-            commonfactors.append(i)
+        if(x % i == 0 and y % i == 0):
+            commonxfactors.append(i)
 
 
-def yfactors():
-    for i in range(1, y + 1):
-        if(y % i == 0):
-            commonfactors.append(i)
-yfactors()
+
+
 xfactors()
+
+
+for i in commonxfactors:
+    if i == i in commonxfactors:
+        gcfah.append(i)
+ 
+gcf = max(gcfah)
+print(gcf)
