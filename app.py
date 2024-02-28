f4409ded39493db1633985f0c3e4b6e07308aa5b
Azrafdabomb <azrafh@nycstudents.net>
2024-02-28 13:51:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 y = int(input("Number 1: "))
 x = int(input("Number 2: "))
 
-
 def gcf():
     y = int(input("Number 1: "))
     x = int(input("Number 2: "))
@@ -14,11 +13,7 @@ def xfactors():
         if(x % i == 0 and y % i == 0):
             commonxfactors.append(i)
 
-
-
-
 xfactors()
-
 
 for i in commonxfactors:
     if i == i in commonxfactors:
